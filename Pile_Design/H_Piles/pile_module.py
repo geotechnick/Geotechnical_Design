12dd131a398b62f658bd221be8dd0ad6e911a328
geotechnick <geotechnick.all@gmail.com>
2024-05-07 08:48:04 -0400

    further plotly altering

--- Pile_Design/H_Piles/pile_module.py
+++ Pile_Design/H_Piles/pile_module.py
@@ -1,13 +1,26 @@
-import base64
 import pandas as pd
 import io
+import base64
 import dash
-from dash import dcc, html, Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
-from dash.dependencies import State
+
+# Load CSV with all the H-Pile info
+H_Pile = pd.read_csv("https://raw.githubusercontent.com/geotechnick/Geotechnical_Design/main/Pile_Design/H_Piles/HP_Spec_Table.csv")
+Fy = 50  # Pile yield stress in ksi
+pile_names = H_Pile['Section_E'].unique().tolist()
+
+# Filter out the metric values
+H_Pile = H_Pile.loc[:, ~H_Pile.columns.str.endswith('_M')]
+
+# Create drop down with the pile names
+dropdown = dcc.Dropdown(id='pile-dropdown', options=[{'label': name, 'value': name} for name in pile_names], value=pile_names[0])
 
 
-def display_row(selected_value): # function for pile info
+#pile selection (note that this code only accounts for one case)##############################
+def display_row(selected_value, H_Pile):
     if selected_value is None:
         return html.Div("No pile selected")
     
@@ -22,6 +35,39 @@ def display_row(selected_value): # function for pile info
     pile_info.reset_index(inplace=True)
 
     return html.Table([html.Tr([html.Th(col), html.Td(pile_display[col].values[0])]) for col in pile_display.columns])
+##################################################################################################
 
-def test_call(value):
-    return value
+#importing soil csv ###########################################################
+def parse_contents(contents, filename):
+    content_type, content_string = contents.split(',')
+
+    decoded = io.StringIO(base64.b64decode(content_string).decode('utf-8'))
+
+    df = pd.read_csv(decoded)
+    
+    # Create Plotly table
+    table = go.Figure(data=[go.Table(
+        header=dict(values=df.columns,
+                    fill_color='paleturquoise',
+                    align='left'),
+        cells=dict(values=[df[column] for column in df.columns],
+                   fill_color='lavender',
+                   align='left'))
+    ])
+    
+    return html.Div([
+        html.H5(filename),
+        html.H6("Raw Data:"),
+        html.Pre(df.to_string()),
+        html.H6("Interactive Table:"),
+        dcc.Graph(
+            id='table',
+            figure=table
+        ),
+    ])
+
+def update_output(contents, filename):
+    if contents is not None:
+        children = parse_contents(contents, filename)
+        return children
+################################################################################
